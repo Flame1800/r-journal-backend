c59efdfb3295f244c23b2db43f2fb79415504b1e
Руслан Рамильевич Мухаметшафиков <rrm@first.local>
2021-10-04 16:02:31 +0500

    init backend

---
+++ src/user/entities/user.entity.ts
@@ -0,0 +1,27 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+} from 'typeorm';
+
+@Entity('users')
+export class UserEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  fullName: string;
+
+  @Column()
+  email: string;
+
+  @Column({ nullable: true })
+  password?: string;
+
+  @CreateDateColumn({ type: 'timestamp' })
+  createdAt: Date;
+
+  @CreateDateColumn({ type: 'timestamp' })
+  updatedAt: Date;
+}
---
+++ src/user/user.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserEntity } from './entities/user.entity';
+import { Repository } from 'typeorm';
+import { FindOneOptions } from 'typeorm/find-options/FindOneOptions';
+import { LoginUserDto } from './dto/login-user.dto';
+
+@Injectable()
+export class UserService {
+  constructor(
+    @InjectRepository(UserEntity)
+    private repository: Repository<UserEntity>,
+  ) {}
+
+  create(dto: CreateUserDto) {
+    return this.repository.save(dto);
+  }
+
+  findAll() {
+    return this.repository.find();
+  }
+
+  findById(id: number) {
+    return this.repository.findOne(id);
+  }
+
+  findByCond(cond: LoginUserDto) {
+    return this.repository.findOne(cond);
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    return `This action updates a #${id} user`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} user`;
+  }
+}
